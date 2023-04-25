236c1bd908dbed90f25c272aa330934da01a385d
EngineerPehr <dragontoothforge@gmail.com>
2023-04-25 10:17:29 -0600

    Optimized books.js

--- public/src/books.js
+++ public/src/books.js
@@ -1,27 +1,29 @@
+// Finds first author that matches given id
 const findAuthorById = (authors = [], id = '') => authors.find((author) => author.id === id)
 
+// Finds first book that matches given id
 const findBookById = (books = [], id = '') => books.find((book) => book.id === id)
 
+// Filters books based on the returned status of the first borrow
 const partitionBooksByBorrowedStatus = (books = []) => {
   const booksOut = books.filter((book) => book.borrows[0].returned === false)
   const booksIn = books.filter((book) => book.borrows[0].returned === true)
   return [booksOut, booksIn]
 }
 
+// Helper function that links accounts to their associated borrows
 const addAccountsToBorrows = (book = {}, accounts = []) => {
-  const updatedBorrows = []
-  const borrowList = book.borrows
-  borrowList.forEach((borrow) => {
-    const idToMatch = borrow.id
+  // Loops through each borrow and account
+  return book.borrows.reduce((acc, borrow) => {
     accounts.forEach((account) => {
-      const accountId = account.id
-      if (idToMatch === accountId) {
+      if (borrow.id === account.id) {
+        // Creates and collects new object if the IDs match
         const updatedBorrow = {...borrow, ...account}
-        updatedBorrows.push(updatedBorrow)
+        acc.push(updatedBorrow)
       }
     })
-  })
-  return updatedBorrows
+    return acc
+  }, [])
 }
 
 const getBorrowersForBook = (book = {}, accounts = []) => addAccountsToBorrows(book, accounts).slice(0, 10)
